bcc46a9ee330b88d567a164c2a3dc61a103f8e60
NonoM33 <renaudlemagicien@gmail.com>
2023-11-20 17:19:36 +0100

    add weatherGlobal rest Object
    update global

---
+++ Sources/WeatherPackage/DATA/RESTWeatherGlobal.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by renaud on 20/11/2023.
+//
+
+import Foundation
+
+public struct RESTWeatherGlobal {
+   public var RESTWeatherCity: RESTWeatherCity?
+   public var RESTDetaillWeather: RESTDetaillWeather?
+}
