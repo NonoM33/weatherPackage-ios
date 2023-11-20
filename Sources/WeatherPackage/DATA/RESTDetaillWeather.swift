d10619293b48f0ee5503a03ba0b47ea8781a89bb
NonoM33 <renaudlemagicien@gmail.com>
2023-11-20 17:15:57 +0100

    add weatherDetail rest Object

---
+++ Sources/WeatherPackage/DATA/RESTDetaillWeather.swift
@@ -0,0 +1,69 @@
+//
+//  File.swift
+//  
+//
+//  Created by renaud on 15/11/2023.
+//
+
+import Foundation
+
+public struct RESTDetaillWeather: Codable {
+    public let lat: Double
+    public let lon: Double
+    public let timezone: String
+    public let timezoneOffset: Int
+    public let current: CurrentWeather
+    public let minutely: [MinutelyWeather]
+
+    public enum CodingKeys: String, CodingKey {
+        case lat, lon, timezone, current, minutely
+        case timezoneOffset = "timezone_offset"
+    }
+}
+
+public struct CurrentWeather: Codable {
+    public let dt: Int
+    public let sunrise: Int
+    public let sunset: Int
+    public let temp: Double
+    public let feelsLike: Double
+    public let pressure: Int
+    public let humidity: Int
+    public let dewPoint: Double
+    public let uvi: Double
+    public let clouds: Int
+    public let visibility: Int
+    public let windSpeed: Double
+    public let windDeg: Int
+    public let weather: [WeatherDetail]
+    public let rain: Rain?
+
+    public enum CodingKeys: String, CodingKey {
+        case dt, sunrise, sunset, temp, pressure, humidity, clouds, visibility, weather, rain
+        case feelsLike = "feels_like"
+        case dewPoint = "dew_point"
+        case windSpeed = "wind_speed"
+        case windDeg = "wind_deg"
+        case uvi
+    }
+}
+
+public struct WeatherDetail: Codable {
+    public let id: Int
+    public let main: String
+    public let description: String
+    public let icon: String
+}
+
+public struct Rain: Codable {
+    public let oneHour: Double
+
+    public enum CodingKeys: String, CodingKey {
+        case oneHour = "1h"
+    }
+}
+
+public struct MinutelyWeather: Codable {
+    public let dt: Int
+    public let precipitation: Double
+}
