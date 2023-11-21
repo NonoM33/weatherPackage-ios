3ce041bbc0284ce8f1cb36e920252e770bcf2ee8
NonoM33 <renaudlemagicien@gmail.com>
2023-11-21 16:41:08 +0100

    update lang, change to FR static

--- Sources/WeatherPackage/API/APIRequest.swift
+++ Sources/WeatherPackage/API/APIRequest.swift
@@ -16,11 +16,11 @@ enum APIRequest {
         switch self {
         case .weather(let latitude, let longitude):
             let apiKey = "0936af5f09c081d03f9836336c844af4"
-            let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&lang=\(languageCode)"
+            let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&lang=fr"
             return URL(string: urlString)
         case .weatherDetail(let latitude, let longitude):
             let apiKey = "0936af5f09c081d03f9836336c844af4"
-            let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&lang=\(languageCode)"
+            let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&lang=fr"
             return URL(string: urlString)
         }
     }
