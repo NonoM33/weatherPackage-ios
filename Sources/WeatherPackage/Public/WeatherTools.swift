7b32bcd7337f69ad5e025c67dd9c2f63d070404a
NonoM33 <renaudlemagicien@gmail.com>
2023-11-21 15:41:54 +0100

    update colors background temperature

--- Sources/WeatherPackage/Public/WeatherTools.swift
+++ Sources/WeatherPackage/Public/WeatherTools.swift
@@ -10,24 +10,38 @@ import UIKit
 
 public class WeatherTools {
 
+    public static func gradientLayerForTemperature(kelvin: Double, frame: CGRect) -> CAGradientLayer {
+        let topColor = colorForTemperature(kelvin: kelvin)
+        let bottomColor = UIColor.white
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = frame
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+
+        return gradientLayer
+    }
+
     public static func colorForTemperature(kelvin: Double) -> UIColor {
         let celsius = kelvin - 273.15
 
         switch celsius {
         case ..<0:
-            return UIColor(red: 0, green: 0, blue: 0.55 + 0.45 * (celsius + 50) / 50, alpha: 1)
+            return UIColor(red: 0, green: 0, blue: max(0.55 + 0.45 * (celsius + 50) / 50 - 0.2, 0), alpha: 1)
         case 0..<10:
-            return UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+            return UIColor(red: 0, green: 0, blue: max(1 - 0.2, 0), alpha: 1)
         case 10..<20:
-            return UIColor(red: 0, green: 1, blue: 1, alpha: 1)
+            return UIColor(red: 0, green: max(1 - 0.2, 0), blue: max(1 - 0.2, 0), alpha: 1)
         case 20..<30:
-            return UIColor(red: 0.5, green: 1, blue: 0.5, alpha: 1)
+            return UIColor(red: max(0.5 - 0.2, 0), green: max(1 - 0.2, 0), blue: max(0.5 - 0.2, 0), alpha: 1)
         case 30..<40:
-            return UIColor(red: 1, green: 1, blue: 0, alpha: 1)
+            return UIColor(red: max(1 - 0.2, 0), green: max(1 - 0.2, 0), blue: 0, alpha: 1)
         case 40...:
-            return UIColor(red: 1, green: 0.45 - 0.45 * (celsius - 40) / 10, blue: 0, alpha: 1)
+            return UIColor(red: 1, green: max(0.45 - 0.45 * (celsius - 40) / 10 - 0.2, 0), blue: 0, alpha: 1)
         default:
-            return UIColor.white
+            return UIColor.gray
         }
     }
 
